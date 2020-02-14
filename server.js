83208edf3dc81e871f03166e68614a01f81ebd72
hbh7 <hbh7@hbh7.com>
2020-02-14 15:26:15 -0500

    Basic image handling, web dir setup

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Welcome to the smart thermostat!'))
+app.get('/', function(req, res) {
+	res.send('Welcome to the smart thermostat!');
+	//res.sendFile('www/' + req.params.file);
+});
+
+app.get('*.png', function(req, res) {
+	
+	res.sendFile('www/' + req.params.file);
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
